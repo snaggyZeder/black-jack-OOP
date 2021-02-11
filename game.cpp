34d22636a0a441410773852c2ccb159a1c71b4de
gg2k5 <georg2k5@rambler.ru>
2021-02-11 18:25:38 +0300

    do 2 players linear game.have problems with score

--- game.cpp
+++ game.cpp
@@ -7,104 +7,6 @@
 #include "game.h"
 using namespace std;
 
-void Game::gaming(unsigned int& playerWin, unsigned int& dealerWin, unsigned int& countDraw, unsigned int& countGame, unsigned int& playerWin2) {
-	Deck my_deck;
-	my_deck.shuffle();
-	//my_deck.print_deck();
-	Player player1;
-	Player player2;
-	Dealer dealer;
-	Hand::GameStatus currentGameStatus = Hand::GAME_CONTINUE;
-	char playing = 'a';
-
-	while (true) {
-
-		if (currentGameStatus == Hand::GAME_CONTINUE) {
-			cout << "player1 your turn" << endl;
-			player1.playp1(my_deck);
-			currentGameStatus = player1.checkGameStatus();
-			
-
-			if (currentGameStatus == Hand::WIN_) {
-				cout << "Player1 win!" << endl; cout << endl; playerWin++;
-				break;
-			}
-			
-
-		else	if (currentGameStatus == Hand::LOOSE_) {
-				cout << "Player1 lose!" << endl; cout << endl; dealerWin++; playerWin2++;
-					break;
-				}
-
-	           else	if (currentGameStatus == Hand::GAME_CONTINUE) {
-						cout << "player2 your turn!" << endl;
-						player2.playp2(my_deck);
-						currentGameStatus = player2.checkGameStatus();
-
-
-						if (currentGameStatus == Hand::WIN_) {
-							cout << "Player2 win!" << endl; cout << endl; playerWin2++;
-							break;
-						}
-						else
-
-							if (currentGameStatus == Hand::LOOSE_) {
-								cout << "Player2 lose!" << endl; cout << endl; dealerWin++; playerWin++;
-								break;
-							}
-							
-
-						cout << "dealer plays" << endl;
-						dealer.playd(my_deck);
-						currentGameStatus = dealer.checkGameStatus();
-						
-						if (currentGameStatus == Hand::GAME_CONTINUE) {
-
-							int playerScore1 = player1.calculateScore();
-							int dealerScore = dealer.calculateScore();
-							int playerScore2 = player2.calculateScore();
-
-
-							if (playerScore2 < playerScore1 > dealerScore) {
-								cout << "Player1 win!" << endl; cout << endl; playerWin++;
-								break;
-							}
-							else if (playerScore1 < playerScore2 > dealerScore) {
-								cout << "Player2 win!" << endl; cout << endl; playerWin2++;
-								break;
-							}
-
-							else if (playerScore2 < dealerScore > playerScore1) {
-								cout << "Dealer win!" << endl; cout << endl;  dealerWin++;
-								break;
-							}
-							else
-								if (dealerScore == playerScore1 && dealerScore == playerScore2 && playerScore1 == playerScore2) {
-									cout << "Draw!" << endl; cout << endl; countDraw++;
-									break;
-								}
-						}
-						else
-
-							if (currentGameStatus == Hand::WIN_) {
-								cout << "Dealer win!" << endl; cout << endl;  dealerWin++;
-								break;
-							}
-							else
-
-								if (currentGameStatus == Hand::LOOSE_) {
-									cout << "Dealer lose!" << endl; cout << endl; playerWin++; playerWin2++;
-									break;
-								}
-
-
-					}
-		}
-	}
-	countGame++;
-}
-
-
 //void Game::gaming(unsigned int& playerWin, unsigned int& dealerWin, unsigned int& countDraw, unsigned int& countGame, unsigned int& playerWin2) {
 //	Deck my_deck;
 //	my_deck.shuffle();
@@ -114,34 +16,27 @@ void Game::gaming(unsigned int& playerWin, unsigned int& dealerWin, unsigned int
 //	Dealer dealer;
 //	Hand::GameStatus currentGameStatus = Hand::GAME_CONTINUE;
 //	char playing = 'a';
-//	bool game = true;
-//	bool pl1 = true; bool pl2 = true; bool dl = true;
 //
-//	int playerScore1 = player1.calculateScore();
-//	int dealerScore = dealer.calculateScore();
+//	while (true) {
 //
-//	using namespace std;	int playerScore2 = player2.calculateScore();
-//
-//	while (game) {
 //		if (currentGameStatus == Hand::GAME_CONTINUE) {
-//			while (pl1 == true) {
+//			cout << "player1 your turn" << endl;
+//			player1.playp1(my_deck);
+//			currentGameStatus = player1.checkGameStatus();
+//			
 //
-//				cout << "player1 your turn" << endl;
-//				player1.playp1(my_deck);
-//				currentGameStatus = player1.checkGameStatus();
+//			if (currentGameStatus == Hand::WIN_) {
+//				cout << "Player1 win!" << endl; cout << endl; playerWin++;
+//				break;
+//			}
+//			
 //
-//				if (currentGameStatus == Hand::WIN_) {
-//					cout << "Player1 win!" << endl; cout << endl; playerWin++;
-//					pl1 = false; game = false;
+//		else	if (currentGameStatus == Hand::LOOSE_) {
+//				cout << "Player1 lose!" << endl; cout << endl; dealerWin++; playerWin2++;
+//					break;
 //				}
-//				else if (currentGameStatus == Hand::LOOSE_) {
-//					cout << "Player1 lose!" << endl; cout << endl;
-//					pl1 = false;
-//				}
 //
-//				////
-//				if (currentGameStatus == Hand::GAME_CONTINUE) {
-//					while (pl2 == true) {
+//	           else	if (currentGameStatus == Hand::GAME_CONTINUE) {
 //						cout << "player2 your turn!" << endl;
 //						player2.playp2(my_deck);
 //						currentGameStatus = player2.checkGameStatus();
@@ -149,42 +44,180 @@ void Game::gaming(unsigned int& playerWin, unsigned int& dealerWin, unsigned int
 //
 //						if (currentGameStatus == Hand::WIN_) {
 //							cout << "Player2 win!" << endl; cout << endl; playerWin2++;
-//							pl1 = false; game = false;
+//							break;
 //						}
 //						else
 //
 //							if (currentGameStatus == Hand::LOOSE_) {
-//								cout << "Player2 lose!" << endl; cout << endl;
-//								pl2 = false;
+//								cout << "Player2 lose!" << endl; cout << endl; dealerWin++; playerWin++;
+//								break;
 //							}
-//						/////
-//							else if (currentGameStatus == Hand::GAME_CONTINUE) {
-//								while (dl == true) {
-//									cout << "dealer plays" << endl;
-//									dealer.playd(my_deck);
-//									currentGameStatus = dealer.checkGameStatus();
+//							
 //
-//									if (currentGameStatus == Hand::WIN_) {
-//										cout << "Dealer win!" << endl; cout << endl; dealerWin++;
-//										dl = false; game = false;
-//									}
-//									else if (currentGameStatus == Hand::LOOSE_) {
-//										cout << "dealer lose!" << endl; cout << endl;
-//										dl = false; game = false;
-//									}
-//									///
+//						cout << "dealer plays" << endl;
+//						dealer.playd(my_deck);
+//						currentGameStatus = dealer.checkGameStatus();
+//						
+//						if (currentGameStatus == Hand::GAME_CONTINUE) {
+//
+//							int playerScore1 = player1.calculateScore();
+//							int dealerScore = dealer.calculateScore();
+//							int playerScore2 = player2.calculateScore();
+//
+//
+//							if (playerScore2 < playerScore1 > dealerScore) {
+//								cout << "Player1 win!" << endl; cout << endl; playerWin++;
+//								break;
+//							}
+//							else if (playerScore1 < playerScore2 > dealerScore) {
+//								cout << "Player2 win!" << endl; cout << endl; playerWin2++;
+//								break;
+//							}
+//
+//							else if (playerScore2 < dealerScore > playerScore1) {
+//								cout << "Dealer win!" << endl; cout << endl;  dealerWin++;
+//								break;
+//							}
+//							else
+//								if (dealerScore == playerScore1 && dealerScore == playerScore2 && playerScore1 == playerScore2) {
+//									cout << "Draw!" << endl; cout << endl; countDraw++;
+//									break;
 //								}
-//								////
-//							}
-//					}
-//					/*if (dealerScore == playerScore1 && dealerScore == playerScore2 && playerScore1 == playerScore2) {
-//								cout << "Draw!" << endl; cout << endl; countDraw++;
-//								game=false;*/
-//								/*}*/
+//						}
+//						else
 //
-//				}
-//			}
-//			countGame++;
+//							if (currentGameStatus == Hand::WIN_) {
+//								cout << "Dealer win!" << endl; cout << endl;  dealerWin++;
+//								break;
+//							}
+//							else
+//
+//								if (currentGameStatus == Hand::LOOSE_) {
+//									cout << "Dealer lose!" << endl; cout << endl; playerWin++; playerWin2++;
+//									break;
+//								}
+//
+//
+//					}
 //		}
 //	}
+//	countGame++;
 //}
+
+
+void Game::gaming(unsigned int& playerWin, unsigned int& dealerWin, unsigned int& countDraw, unsigned int& countGame, unsigned int& playerWin2) {
+	Deck my_deck;
+	my_deck.shuffle();
+	//my_deck.print_deck();
+	Player player1;
+	Player player2;
+	Dealer dealer;
+
+	int playerScore1=0; int dealerScore=0; int playerScore2 = 0;
+
+	Hand::GameStatus currentGameStatus1 = Hand::GAME_CONTINUE;
+	Hand::GameStatus currentGameStatus2 = Hand::GAME_CONTINUE;
+	Hand::GameStatus currentGameStatusd = Hand::GAME_CONTINUE;
+
+	char playing = 'a';
+	bool game = true;
+	bool pl1 = true; bool pl2 = true; bool dl = true;
+
+
+
+	using namespace std;
+
+	while (pl1 == true) {
+
+		cout << "player1 your turn!" << endl;
+		player1.playp1(my_deck);
+		currentGameStatus1 = player1.checkGameStatus();
+
+		if (currentGameStatus1 == Hand::WIN_) {
+			cout << "Player1 win!" << endl; cout << endl; playerWin++;
+			pl1 = false;
+		}
+		else if (currentGameStatus1 == Hand::LOOSE_) {
+				cout << "Player1 lose!" << endl; cout << endl;
+				/*pl1 = false;*/
+				playerScore1 = 0;
+			}
+			if (currentGameStatus1 == Hand::GAME_CONTINUE || currentGameStatus1 == Hand::LOOSE_) {
+				
+			cout << "player2 your turn!" << endl;
+			player2.playp2(my_deck);
+			currentGameStatus2 = player2.checkGameStatus();
+
+
+			if (currentGameStatus2 == Hand::WIN_) {
+				cout << "Player2 win!" << endl; cout << endl; playerWin2++;
+				 pl1 = false;
+			}
+			else if (currentGameStatus2 == Hand::LOOSE_) {
+				cout << "Player2 lose!" << endl; cout << endl;
+				playerScore2 = 0;
+			}
+			else if (currentGameStatus1 == Hand::LOOSE_ && currentGameStatus2 == Hand::LOOSE_) {
+				cout << "Dealer win !" << endl; pl1 = false; dealerWin++;
+			}
+
+
+			/*else  if (currentGameStatus2== Hand::GAME_CONTINUE|| currentGameStatus2 == Hand::LOOSE_) {*/
+
+				cout << "dealer your turn!" << endl;
+				dealer.playd(my_deck);
+				currentGameStatusd = dealer.checkGameStatus();
+
+				playerScore1 = player1.calculateScore();
+				dealerScore = dealer.calculateScore();
+				playerScore2 = player2.calculateScore();
+
+				if (currentGameStatusd == Hand::WIN_) {
+					cout << "Dealer win!" << endl; cout << endl; dealerWin++;
+					/*dl = false;*//* pl2 = false;*/ pl1 = false;
+				}
+				else if (currentGameStatusd == Hand::LOOSE_) {
+					cout << "dealer lose!" << endl; cout << endl;
+					/*dl = false; pl2 = false;*/pl1 = false;
+				}
+
+				///
+				else if (currentGameStatusd == Hand::LOOSE_ && currentGameStatus2 == Hand::LOOSE_) {
+					cout << "player1  wins!" << endl; cout << endl;playerWin++; pl1 = false;
+				}
+
+				else if (currentGameStatusd == Hand::LOOSE_ && currentGameStatus1 == Hand::LOOSE_) {
+					cout << "player2  wins!" << endl; cout << endl; playerWin2++; pl1 = false;
+				}
+
+				////
+				else if (dealerScore == playerScore1 && dealerScore == playerScore2 && playerScore1 == playerScore2) {
+					cout << "Draw!" << endl; cout << endl; countDraw++; pl1 = false;
+				}
+				else if (dealerScore > playerScore1 && dealerScore > playerScore2) {
+					cout << "Dealer wins!" << endl; cout << endl; dealerWin++; pl1 = false;
+				}
+				else if (playerScore1 > dealerScore && playerScore1 > playerScore2) {
+					cout << "Player1  wins!" << endl; cout << endl; playerWin++; pl1 = false;
+				}
+
+				else if (playerScore2 > dealerScore && playerScore2 > playerScore1) {
+					cout << "Player2  wins!" << endl; cout << endl; playerWin2++; pl1 = false;
+				}
+
+
+			
+
+				////
+
+			}
+
+
+
+			/////
+
+		}
+	
+			countGame++;
+		
+	}
--- player.cpp
+++ player.cpp
@@ -9,19 +9,12 @@ using namespace std;
 
 
 void Player::playp1(Deck& fDeck) {
-	char choice = '~';
+	char choice = 'y';
 	int score = 0;
 	Hand::GameStatus currentGameStatus = Hand::GAME_CONTINUE;
 
-	takeOneCard(fDeck);
-	printHand();
-	score = calculateScore();
-	cout << "Player1 summ is =" << score << endl;
-	cout << endl;
 
-	do { cout << " (enter your choice, y - yes, n - no) ";  cin >> choice; } while (choice != 'y' && choice != 'n');
-
-	if (choice == 'n') { cout << "its final sum of your cards player1 =" << score << endl; cout << endl; }
+	
 
 	while (choice == 'y') {
 		takeOneCard(fDeck);
@@ -40,19 +33,14 @@ void Player::playp1(Deck& fDeck) {
 
 
 void Player::playp2(Deck& fDeck) {
-	char choice = '~';
+	char choice = 'y';
 	int score = 0;
 	Hand::GameStatus currentGameStatus = Hand::GAME_CONTINUE;
 
-	takeOneCard(fDeck);
-	printHand();
-	score = calculateScore();
-	cout << "Player2 summ is =" << score << endl;
-	cout << endl;
+	
 
-	do { cout << " (enter your choice, y - yes, n - no) ";  cin >> choice; } while (choice != 'y' && choice != 'n');
+	
 
-	if (choice == 'n') { cout << "its final sum of your cards player2 =" << score << endl; cout << endl; }
 
 	while (choice == 'y') {
 		takeOneCard(fDeck);
